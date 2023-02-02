9cc336ff5e15a760472fe2ec44c633b54fbdbd5d
Steve Rector <steverector27@gmail.com>
2023-02-01 17:02:06 -0800

    Add TextMeshPro and implement robo dog

--- Assets/Scripts/Dog.cs
+++ Assets/Scripts/Dog.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Dog : MonoBehaviour
 {
+    // ** QUICK DEFINITIONS **
+    // the protected keyword lets the children of a parent class use the protected member, so any child of Dog can use that "Bark Bark" string
+    // the virtual keyword lets the children of a parent class override the functionality of the virtual method as needed, so if a Dog chil barks a little differently than most 
+    // we can override the behavior as needed.
+    // ** End **
+
     protected string bark = "Bark Bark!";
+
     public virtual void Start()
     {
         Bark();
     }
 
+    /// <summary>
+    /// Prints out the Dog's bark to the console along with its name
+    /// </summary>
     public virtual void Bark()
     {
-        // transform.name gets the name of the game object 
-        Debug.Log($" {transform.name} goes {bark}");
+        Debug.Log($" {gameObject.name} goes {bark}");
     }
 }
